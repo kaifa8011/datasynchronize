d807ae0f202b6b23d2b948a892b336bbb8a1c021
songzi <songzi@ecook.cn>
2021-04-19 10:52:02 +0800

    调整url格式

--- src/main/java/com/ciba/data/synchronize/sample/manager/SampleUrlManager.java
+++ src/main/java/com/ciba/data/synchronize/sample/manager/SampleUrlManager.java
@@ -1,7 +1,6 @@
 package com.ciba.data.synchronize.sample.manager;
 
 import android.text.TextUtils;
-import android.util.Log;
 
 import com.ciba.data.synchronize.util.SPUtil;
 
@@ -21,7 +20,7 @@ public class SampleUrlManager {
      */
     public static final String DYNAMIC_DOMAIN_KEY = "dynamic_domain";
 
-    private static final String DEFAULT_BASE_URL = "http://dc.114dev.com/";
+    private static final String DEFAULT_BASE_URL = "http://dc.114dev.com";
     //    private static final String SAMPLE_OPERATION_DATA_URL = DEFAULT_BASE_URL + "log/secOplog.json";
 //    private static final String SAMPLE_DEVICE_DATA_URL = DEFAULT_BASE_URL + "log/getToken.json";
 //    private static final String SAMPLE_INSTALL_DATA_URL = DEFAULT_BASE_URL + "log/inrlog.json";
@@ -45,50 +44,49 @@ public class SampleUrlManager {
     public static String getDeviceDataUrl() {
 //        getDeviceDataUrl
         String host = getRequestHost();
-        String url = host + "log/getToken.json";
-        Log.e("wsong", "logext host url :" + url);
+        String url = host + "/log/getToken.json";
         return url;
     }
 
     public static String getInstallDataUrl() {
 //        getInstallDataUrl();
         String host = getRequestHost();
-        String url = host + "log/inrlog.json";
+        String url = host + "/log/inrlog.json";
         return url;
     }
 
     public static String getIPV6PostUrl() {
 //      IPV6_INFO_POST_URL;
         String host = getRequestHost();
-        String url = host + "log/ip/log";
+        String url = host + "/log/ip/log";
         return url;
     }
 
     public static String getStartUpDataUrl() {
 //        getStartUpDataUrl();
         String host = getRequestHost();
-        String url = host + "log/strlog.json";
+        String url = host + "/log/strlog.json";
         return url;
     }
 
     public static String getCrashUrl() {
 //      getCrashDataUrl();
         String host = getRequestHost();
-        String url = host + "log/secOplog.json";
+        String url = host + "/log/secOplog.json";
         return url;
     }
 
     public static String getActivityLifeUrl() {
         //        getActivityLifeUrl();
         String host = getRequestHost();
-        String url = host + "log/secOplog.json";
+        String url = host + "/log/secOplog.json";
         return url;
     }
 
     public static String getMotionEventUrl() {
 //       getMotionEventUrl();
         String host = getRequestHost();
-        String url = host + "log/secOplog.json";
+        String url = host + "/log/secOplog.json";
         return url;
     }
 
