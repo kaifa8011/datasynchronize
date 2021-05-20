0923eb807e5dfcb7265c64c5d19e6ed76a6e10a5
songzi <songzi@ecook.cn>
2021-05-20 16:58:21 +0800

    增加时间间隔控制类

---
+++ src/main/java/com/ciba/data/synchronize/util/InstallListReadTimeController.java
@@ -0,0 +1,51 @@
+package com.ciba.data.synchronize.util;
+
+import android.util.Log;
+
+import com.ciba.data.synchronize.util.SPUtil;
+
+/**
+ * @author songzi
+ * @date 2021/5/20
+ */
+public class InstallListReadTimeController {
+
+    /**
+     * 安装列表读取间隔
+     */
+    private static final long INSTALL_LIST_READ_TIME_INTERVAL_TIME = 60 * 1000 * 24 * 3;
+    /**
+     * 安装列表KEY
+     */
+    public static final String KEY_INSTALL_LIST_READ_TIME = "LOG_INSTALL_LIST_READ_TIME";
+
+
+    /**
+     * 根据本地配置信息判断是否可读取手机安装列表
+     *
+     * @return
+     */
+    public static boolean isLocalInstallListRead() {
+        // 获取最后一次安装列表读取时间
+
+        Long lastInstallListReadTime = SPUtil.getLong(KEY_INSTALL_LIST_READ_TIME);
+        // 当前时间
+        Long timeMillis = System.currentTimeMillis();
+        // 当前上传间隔时长是否小于INSTALL_LIST_READ_TIME_INTERVAL_TIME间隔
+        if (timeMillis - lastInstallListReadTime < INSTALL_LIST_READ_TIME_INTERVAL_TIME) {
+            return false;
+        }
+        return true;
+    }
+
+
+    /**
+     * 更新本地上传安装列表时间
+     */
+    public static void saveLocalInstallListReadTime() {
+        Long installListReadTime = System.currentTimeMillis();
+        SPUtil.putLong(KEY_INSTALL_LIST_READ_TIME, installListReadTime);
+    }
+
+
+}
